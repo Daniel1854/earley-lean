77e4fa2b3a501a7a9faec97410364b979fa244d3
Daniel Soukup <d.soukup@campus.lmu.de>
2026-05-08 13:53:27 +0200

    feat: split soundness up

--- Earley/Earley.lean
+++ Earley/Earley.lean
@@ -176,8 +176,8 @@ public inductive EarleySet (G : ContextFreeGrammar T) (w : List (Symbol T G.NT))
   introduces an EarleyItem where that rule gets followed through.
   -/
   | predict (x : EarleyItem T G.NT) (rule1 rule2 : ContextFreeRule T G.NT) (pos i j : Nat)
-      (a : Symbol T G.NT) (hx : x = ⟨rule1,pos,i,j⟩) (hmem : x ∈ EarleySet G w)
-      (hr2 : rule2 ∈ G.rules) (hnext : nextSymbol x = some (Symbol.nonterminal rule2.input))
+      (a : Symbol T G.NT) (hx : x = ⟨rule1,pos,i,j⟩) (hmemx : x ∈ EarleySet G w)
+      (hmemr2 : rule2 ∈ G.rules) (hnext : nextSymbol x = some (Symbol.nonterminal rule2.input))
       (hbounds : j < w.length) (hw : w[j] = a)
       : EarleySet G w ⟨rule2,0,j,j⟩
   /-
--- Earley/Invariants.lean
+++ Earley/Invariants.lean
@@ -13,33 +13,28 @@ namespace Invariants
 
 section EarleySet
 
-variable {T : Type} {N : Type} [BEq T]
+variable {T : Type} {N : Type}
 
-omit [BEq T] in
 @[simp]
 lemma alphaItem_of_zero (item : EarleyItem T N) (h : item.position = 0) :
     alphaItem item = [] := by
   simp [alphaItem, h]
 
-omit [BEq T] in
 @[simp]
 lemma betaItem_of_zero (item : EarleyItem T N) (h : item.position = 0) :
     betaItem item = item.rule.output := by
   simp [betaItem, h]
 
-omit [BEq T] in
 @[simp]
 lemma alphaItem_of_len (item : EarleyItem T N) (h : item.position = item.rule.output.length) :
     alphaItem item = item.rule.output := by
   simp [alphaItem, h]
 
-omit [BEq T] in
 @[simp]
 lemma betaItem_of_len (item : EarleyItem T N) (h : item.position = item.rule.output.length) :
     betaItem item = [] := by
   simp [betaItem, h]
 
-omit [BEq T] in
 /--
 If there is a next symbol, then the position `pos+1` is still in bounds of the rhs of the rule.
 -/
@@ -49,9 +44,9 @@ lemma bounds_of_nextSymbol_eq_some {G : ContextFreeGrammar T} {x : EarleyItem T 
   have := of_getElem?_eq_some h
   omega
 
-omit [BEq T] in
 /--
 Any EarleyItem within an EarleySet is well-formed.
+TODO: maybe split these up like I did with `soundItemEarley`?
 -/
 public theorem wfEarley (G : ContextFreeGrammar T) [BEq G.NT] (w : List (Symbol T G.NT))
     (x : EarleyItem T G.NT) (hmem : x ∈ EarleySet G w) : isWellFormed G w x := by
@@ -77,7 +72,6 @@ public theorem wfEarley (G : ContextFreeGrammar T) [BEq G.NT] (w : List (Symbol 
 open ContextFreeRule
 open ContextFreeGrammar
 
-omit [BEq T] in
 /--
 Any EarleyItem within an EarleySet produced through the .init constructor is sound.
 Since we are the beginning of the rule, we only need to show that the initial NT
@@ -91,61 +85,86 @@ public theorem soundItemInit (G : ContextFreeGrammar T) [BEq G.NT] (w : List (Sy
   use rule
   simp [hmem, Rewrites.input_output]
 
-omit [BEq T] in
+/--
+Any EarleyItem within an EarleySet produced through the .scan constructor is sound.
+Since we know that the next symbol is `a`, which matches the next symbol in our rule (β) as well,
+we can simply construct a β' from β = a β' and showcase that the production holds
+-/
+public theorem soundItemScan (G : ContextFreeGrammar T) [BEq G.NT] (w : List (Symbol T G.NT))
+    {x : EarleyItem T G.NT} {rule : ContextFreeRule T G.NT} {pos i j : Nat} {a : Symbol T G.NT}
+    (hx : x = ⟨rule, pos, i, j⟩) (hmem : x ∈ EarleySet G w) (hbounds : j < w.length)
+    (hw : w[j] = a) (hnext : nextSymbol x = some a) : isSound G w ⟨rule,pos+1,i,j+1⟩ := by
+  unfold isSound
+  simp only
+  --simp only [hx] at ih
+  let y : EarleyItem T G.NT := ⟨rule,pos+1,i,j+1⟩
+  have : a :: betaItem y = betaItem x :=  by
+    simp only [betaItem]
+    simp only [nextSymbol, hx] at hnext
+    have := List.getElem_of_getElem? hnext
+    rcases this with ⟨w,hpos⟩
+    have := @List.getElem_cons_drop _ rule.output pos w
+    rw [hpos] at this
+    simp [this,hx,y]
+  have : w.extract i (j+1) = w.extract i j ++ [w[j]] := by
+    simp
+    sorry
+  -- ⊢ List.take (j + 1 - i) (List.drop i w) = List.take (j - i) (List.drop i w) ++ [w[j]]
+  -- List.take (x+1) hl = List.take (x) hl ++ hl[x]
+  --have := List.take_succ_cons
+  simp [betaItem]
+  simp at this
+  have := @List.take_add  _ (List.drop i w) (j-i) 1
+  -- have "slice \<omega> i j @ \<beta>_item x = slice \<omega> i (j+1) @ \<beta>_item'"
+  simp [Derives]
+  simp_all
+  sorry
+
+/--
+Any EarleyItem within an EarleySet produced through the .predict constructor is sound.
+TODO
+-/
+public theorem soundItemPredict (G : ContextFreeGrammar T) [BEq G.NT] (w : List (Symbol T G.NT))
+    {x : EarleyItem T G.NT} {rule1 rule2 : ContextFreeRule T G.NT} {pos i j : Nat}
+    (hx : x = ⟨rule1, pos, i, j⟩) (hmemx : x ∈ EarleySet G w) (hmemr2 : rule2 ∈ G.rules)
+    (hnext : nextSymbol x = some (Symbol.nonterminal rule2.input)) {a : Symbol T G.NT}
+    (hbounds : j < w.length) (hw : w[j] = a) : isSound G w ⟨rule2,0,j,j⟩ := by
+  unfold isSound
+  simp only
+  sorry
+
+/--
+Any EarleyItem within an EarleySet produced through the .complete constructor is sound.
+TODO
+-/
+public theorem soundItemComplete (G : ContextFreeGrammar T) [BEq G.NT] (w : List (Symbol T G.NT))
+    {x y : EarleyItem T G.NT} {rule1 rule2 : ContextFreeRule T G.NT} {posx posy i j k : Nat}
+    (hx : x = ⟨rule1, posx, i, j⟩) (hmemx : x ∈ EarleySet G w)
+    (hy : y = ⟨rule2, posy, j, k⟩) (hmemy : y ∈ EarleySet G w)
+    (hcomp : isComplete y) (hnext : nextSymbol x = some (Symbol.nonterminal rule2.input)) :
+    isSound G w ⟨rule1,posx+1,i,k⟩ := by
+  unfold isSound
+  simp only
+  have := bounds_of_nextSymbol_eq_some hnext
+  simp only [hx] at this
+  sorry
+
 /--
 Any EarleyItem within an EarleySet is sound.
-TODO: Maybe it makes sense to split these into separate lemmas for reuse.
 -/
 public theorem soundItemEarley (G : ContextFreeGrammar T) [BEq G.NT] (w : List (Symbol T G.NT))
     (x : EarleyItem T G.NT) (hmem : x ∈ EarleySet G w) : isSound G w x := by
   unfold isSound
   induction hmem with
-  | init rule hmem hstart =>
-    apply soundItemInit
-    exact hmem
-  -- SCAN:
-  -- The ih tells us, that up to the current point, we've got a sound item.
-  -- Since we know that the next symbol is a, which matches the next symbol in our rule (β) as well,
-  -- we can simply construct a β' from β = a β' and showcase that the production holds
-  | scan x rule pos i j a hx hmem hbounds hw hnext ih =>
-    simp only
-    simp only [hx] at ih
-    let y : EarleyItem T G.NT := ⟨rule,pos+1,i,j+1⟩
-    have : a :: betaItem y = betaItem x :=  by
-      simp only [betaItem]
-      simp only [nextSymbol, hx] at hnext
-      have := List.getElem_of_getElem? hnext
-      rcases this with ⟨w,hpos⟩
-      have := @List.getElem_cons_drop _ rule.output pos w
-      rw [hpos] at this
-      simp [this,hx,y]
-    have : w.extract i (j+1) = w.extract i j ++ [w[j]] := by
-      simp
-      sorry
-    -- ⊢ List.take (j + 1 - i) (List.drop i w) = List.take (j - i) (List.drop i w) ++ [w[j]]
-    -- List.take (x+1) hl = List.take (x) hl ++ hl[x]
-    --have := List.take_succ_cons
-    simp [betaItem]
-    simp at this
-    have := @List.take_add  _ (List.drop i w) (j-i) 1
-    -- have "slice \<omega> i j @ \<beta>_item x = slice \<omega> i (j+1) @ \<beta>_item'"
-    simp [Derives]
-    simp_all
-    sorry
-  | predict x rule1 rule2 pos i j a hx hmem hr2 hnext hbounds hw ih =>
-    simp only
-    simp only [hx] at ih
-    --refine ⟨hr2,by omega⟩
-    sorry
+  | init _ hmem =>
+    exact soundItemInit _ _ _ hmem
+  | scan _ _ _ _ _ _ hx hmem hbounds hw hnext =>
+    exact soundItemScan _ _ hx hmem hbounds hw hnext
+  | predict _ _ _ _ _ _ _ hx hmemx hmemr2 hnext hbounds hw =>
+    exact soundItemPredict _ _ hx hmemx hmemr2 hnext hbounds hw
   | complete x y rule1 rule2 posx posy i j k hx hmemx hy hmemy hcomp hnext ihx ihy =>
-    simp only
-    have := bounds_of_nextSymbol_eq_some hnext
-    simp only [hx] at ihx this
-    simp only [hy] at ihy
-    --refine ⟨ihx.left,this, by omega⟩
-    sorry
+    exact soundItemComplete _ _ hx hmemx hy hmemy hcomp hnext
 
-omit [BEq T] in
 /--
 The soundness criteria for the EarleySet:
 Given a finished item for a word within the set,
