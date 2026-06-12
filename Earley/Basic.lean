96d0bf1d990b67fd692950a5a2a55056eefeb9c4
Daniel Soukup <d.soukup@campus.lmu.de>
2026-06-12 20:00:31 +0200

    feat: enable grind suggestions

--- Earley/Basic.lean
+++ Earley/Basic.lean
@@ -1,5 +1,6 @@
 module
 public import Mathlib.Computability.ContextFreeGrammar
+public import Lean.LibrarySuggestions.Default
 
 deriving instance BEq for Symbol
 deriving instance BEq for ContextFreeRule
