0c87446c47dd008e125aca2c4e744abe401c8c8b
Daniel Soukup <d.soukup@campus.lmu.de>
2026-06-22 12:09:51 +0200

    chore: move unused theorems to better spot

--- Earley/Recognizer.lean
+++ Earley/Recognizer.lean
@@ -372,6 +372,77 @@ lemma updateBinAux_of_nullPre (xs : List (BinItem T N)) (y : BinItem T N) {i : N
   | nil => grind
   | cons head tail ih => grind
 
+section unused
+
+lemma eqLength_of_updateBinAux_of_mem (xs : List (BinItem T N)) (y : BinItem T N)
+    (hNoDup : (items xs).Nodup) (hmem : y.item ∈ items xs) :
+    (updateBinAux xs y).length = xs.length := by
+  induction xs generalizing y with
+  | nil => grind
+  | cons x xs ih => grind
+
+-- TODO: these are all unused but I can see them becoming useful for concise recognizer proofs
+lemma updateBinAux_of_Red_of_eqItem (xs : List (BinItem T N)) (y : BinItem T N) {i j : Nat}
+    (hNoDup : (items xs).Nodup) {yP : List ReductionPointer} (hy : y.pointer = Pointer.reduction yP)
+    (hi : i < xs.length) (hx : xs[i].pointer = Pointer.null ∨ xs[i].pointer = Pointer.predecessor j)
+    (heq : y.item = xs[i].item) : updateBinAux xs y = xs := by
+  induction xs generalizing i with
+  | nil => grind
+  | cons x xs ih => grind
+
+lemma updateBinAux_of_Red_of_neqItemAux (xs : List (BinItem T N)) (y : BinItem T N) (i : Nat)
+    (hi : i < xs.length) (hneq : y.item ≠ xs[i].item) (hlen : i < (updateBinAux xs y).length) :
+    (updateBinAux xs y)[i] = xs[i] := by
+  induction xs generalizing i with
+  | nil => grind
+  | cons x xs ih => grind
+
+lemma updateBinAux_of_Red_of_neqItem (xs : List (BinItem T N)) (y : BinItem T N) (i j : Nat)
+    (hNoDup : (items xs).Nodup) (hi : i < xs.length) (heq : y.item = xs[i].item) (hneq : i ≠ j)
+    (hj : j < xs.length) (hlen : j < (updateBinAux xs y).length) :
+    (updateBinAux xs y)[j] = xs[j] := by
+  induction xs generalizing i with
+  | nil => grind
+  | cons x xs ih =>
+    have : y.item ≠ (x :: xs)[j].item := by
+      grind [List.nodup_iff_getElem?_ne_getElem?]
+    grind [updateBinAux_of_Red_of_neqItemAux]
+
+theorem updateBinAux_of_updRed (xs xs' : List (BinItem T N)) (y : BinItem T N) (i : Nat)
+    (hNoDup : (items xs).Nodup) {xP yP : List ReductionPointer}
+    (hi : i < xs.length) (hx : xs[i].pointer = Pointer.reduction xP) (heq : y.item = xs[i].item)
+    (hy : y.pointer = Pointer.reduction yP) (hxs' : xs' = updateBinAux xs y) :
+    xs'.length = xs.length ∧ ((hlen : xs'.length = xs.length) →
+    xs'[i].pointer = Pointer.reduction (xP.append yP) ∧
+    (∀ j, (hj : j < xs'.length ∧ i ≠ j) → xs'[j] = xs[j]'(by lia))) := by
+  induction xs generalizing i y xs' with
+  | nil => grind
+  | cons x xs ih =>
+    refine ⟨by grind [eqLength_of_updateBinAux_of_mem], ?_⟩
+    intro hlen
+    simp only [hxs', ne_eq, forall_and_index]
+    constructor
+    · if h : x.item = y.item then
+        clear ih
+        simp only [updateBinAux, beq_iff_eq, List.append_eq]
+        have hxP : x.pointer = Pointer.reduction xP := by grind
+        have hx2 : x = ⟨x.item, Pointer.reduction xP⟩ := by rw [← hxP]
+        have hy2 : y = ⟨x.item, Pointer.reduction yP⟩ := by rw [← hy]; rw [h]
+        grind
+      else
+        have hi : i - 1 < xs.length := by grind
+        specialize ih (updateBinAux xs y) y (i-1) (by grind [List.Nodup.of_cons]) hi
+        have xs1 : xs[i - 1].pointer = Pointer.reduction xP := by grind
+        have h2 : y.item = xs[i - 1].item := by grind
+        specialize ih xs1 h2 hy (by simp)
+        have ⟨h3, _⟩ := ih.right ih.left
+        grind
+    · intro j hk hneq
+      have := updateBinAux_of_Red_of_neqItem (x::xs) y i j
+      grind
+
+end unused
+
 lemma wfBinPointers_of_updateBinAux (w : List T) {k : Nat} (bins : EarleyBins T N (w.length + 1))
     (hk : k < w.length + 1) (xs : List (BinItem T N)) (hwfbin : isWellFormedBinPointers w bins xs k)
     (y : BinItem T N) (hwfy : isWellFormedPointer w bins y.pointer k) :
