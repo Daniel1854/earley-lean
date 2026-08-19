bbb49afb77a5f152603b105f4260af65c35d59bd
Daniel Soukup <d.soukup@campus.lmu.de>
2026-08-19 19:56:59 +0200

    chore: replace manual proof by grind

--- Earley/Proofs/CachedRecognizerPointers.lean
+++ Earley/Proofs/CachedRecognizerPointers.lean
@@ -63,24 +63,7 @@ public theorem earleyBinCached_eq_earleyBinList (G : ContextFreeGrammarList T N)
     have : ¬ List.length bins[k] ≤ j := by grind
     simp only [this, ↓reduceDIte]
     apply earleyBinCached_eq_earleyBinList
-    have hkj : binsCached[k].raw[j].item = bins[k][j].item := by grind
-    match hnext : bins[k][j].item.nextSymbol with
-    | some s => match s with
-      | Symbol.nonterminal A =>
-        have : binsCached[k].raw[j].item.nextSymbol = some (Symbol.nonterminal A) := by grind
-        simp [this, updateBinsCached_eq_updateBins (hbins := hbinsCached), heq]
-      | Symbol.terminal a =>
-        have : binsCached[k].raw[j].item.nextSymbol = some (Symbol.terminal a) := by grind
-        simp only [this]
-        if hk : k ≥ w.size then
-          simp only [hk, ↓reduceDIte, heq]
-        else
-          simp only [hk, ↓reduceDIte, heq, hkj,
-            updateBinsCached_eq_updateBins (hbins := hbinsCached)]
-    | none =>
-      have : binsCached[k].raw[j].item.nextSymbol = none := by grind
-      simp only [this, updateBinsCached_eq_updateBins (hbins := hbinsCached), heq]
-      simp only [hkj]
+    grind [updateBinsCached_eq_updateBins]
 termination_by { x | isWellFormed G.rules w.size x }.ncard + 1 - j
 decreasing_by exact decreasingAux hbins j k (by grind) (by simp only [not_le] at this; exact this)
 
